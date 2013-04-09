36002f1fab6213dd0ee416ea45f596072dc199e6
maxim <max.pestun@gmail.com>
2013-04-09 17:24:23 +0400

    request animation frame method

--- src/main.js
+++ src/main.js
@@ -7,3 +7,24 @@ window.onload =function () {
     core.canvas = document.getElementById("pad");
     core.context = core.canvas.getContext("2d");
 };
+
+// shim layer with setTimeout fallback
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+
+
+// usage:
+// instead of setInterval(render, 16) ....
+
+(function animloop(){
+    requestAnimFrame(animloop);
+    render();
+})();
+// place the rAF *before* the render() to assure as close to
+// 60fps with the setTimeout fallback.

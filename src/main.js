2cf43a97c831b1079babefc4205b9ade230cb1be
maxim <max.pestun@gmail.com>
2013-04-10 17:00:12 +0400

    begin with cursor

--- src/main.js
+++ src/main.js
@@ -58,7 +58,8 @@ window.requestAnimFrame = (function () {
 // ---------------------- GAME OBJECTS ---------------------
 // ---------------------------------------------------------
 var gameObjects = {
-    avatars: []
+    avatars: [],
+    cursor: null
 };
 
 // ---------------------------------------------------------
@@ -108,6 +109,10 @@ Avatar.prototype.draw = function (context) {
     context.stroke();
 };
 
+function Cursor() {
+
+}
+
 // ---------------------------------------------------------
 // -------------------- KEYS PROCESSING --------------------
 // ---------------------------------------------------------
@@ -119,19 +124,35 @@ var keyMap = {
     down: false
 };
 
+function isOnlyArrowKeyPressCorrector() {
+    var numberOfPressedArrowKeys = 0;
+
+    if (keyMap.left) numberOfPressedArrowKeys++;
+    if (keyMap.right) numberOfPressedArrowKeys++;
+    if (keyMap.up) numberOfPressedArrowKeys++;
+    if (keyMap.down) numberOfPressedArrowKeys++;
+
+    if (numberOfPressedArrowKeys <= 1) return 1;
+    else return 1 / Math.sqrt(2);
+}
+
 setInterval(processKeys, settings.keysUpdateInterval);
 function processKeys() {
     if (keyMap.left) {
-        gameObjects.avatars[settings.playersAvatarId].translate(-settings.avatarTranslateSpeed, 0);
+        gameObjects.avatars[settings.playersAvatarId]
+            .translate(-settings.avatarTranslateSpeed * isOnlyArrowKeyPressCorrector(), 0);
     }
     if (keyMap.right) {
-        gameObjects.avatars[settings.playersAvatarId].translate(settings.avatarTranslateSpeed, 0);
+        gameObjects.avatars[settings.playersAvatarId]
+            .translate(settings.avatarTranslateSpeed * isOnlyArrowKeyPressCorrector(), 0);
     }
     if (keyMap.up) {
-        gameObjects.avatars[settings.playersAvatarId].translate(0, -settings.avatarTranslateSpeed);
+        gameObjects.avatars[settings.playersAvatarId]
+            .translate(0, -settings.avatarTranslateSpeed * isOnlyArrowKeyPressCorrector());
     }
     if (keyMap.down) {
-        gameObjects.avatars[settings.playersAvatarId].translate(0, settings.avatarTranslateSpeed);
+        gameObjects.avatars[settings.playersAvatarId]
+            .translate(0, settings.avatarTranslateSpeed * isOnlyArrowKeyPressCorrector());
     }
 }
 
@@ -139,10 +160,8 @@ function processKeys() {
 document.onkeydown = function (event) {
     var keyCode;
 
-    if (event == null)
-        keyCode = window.event.keyCode;
-    else
-        keyCode = event.keyCode;
+    if (event == null) keyCode = window.event.keyCode;
+    else keyCode = event.keyCode;
 
     switch (keyCode) {
         case 37:
@@ -165,10 +184,8 @@ document.onkeydown = function (event) {
 document.onkeyup = function (event) {
     var keyCode;
 
-    if (event == null)
-        keyCode = window.event.keyCode;
-    else
-        keyCode = event.keyCode;
+    if (event == null) keyCode = window.event.keyCode;
+    else keyCode = event.keyCode;
 
     switch (keyCode) {
         case 37:
